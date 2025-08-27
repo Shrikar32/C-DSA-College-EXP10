8f2324ff19b1e17747241e01708d2fe122668f45
Shrikar32 <shrikarmujumdar32@gmail.com>
2025-08-28 00:36:23 +0530

    C3

---
+++ pointerimp3.cpp
@@ -0,0 +1,15 @@
+//call by reference with increment
+#include <iostream>
+using namespace std;
+void swap(int &x ){
+    int aa = 45;
+  x=x+aa;
+ 
+ 
+cout << "value incremented in function  "<< x<<endl;
+}
+int main(){
+ int a=3;
+ cout << "original value  "<< a <<endl;
+ swap(a);
+}
